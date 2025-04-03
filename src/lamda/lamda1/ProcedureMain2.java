08bfbee9c19caa526d99c0be654584dfa047c39c
rndud008 <rndud008@gmail.com>
2025-04-04 00:01:56 +0900

    람다의 시작

---
+++ src/lamda/lamda1/ProcedureMain2.java
@@ -0,0 +1,12 @@
+package lamda.lamda1;
+
+import lamda.Procedure;
+
+public class ProcedureMain2 {
+    public static void main(String[] args) {
+        Procedure procedure = () -> {
+            System.out.println("hello! lamda!");
+        };
+        procedure.run();
+    }
+}
---
+++ src/lamda/start/Ex1RefMain.java
@@ -0,0 +1,74 @@
+package lamda.start;
+
+import lamda.Procedure;
+
+import java.util.Random;
+
+public class Ex1RefMain {
+
+    public static void hello(Procedure procedure){
+        long startNs = System.nanoTime();
+
+        // 코드 조각 시작
+       procedure.run();
+        //코드 조각 종료
+
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간: " + (endNs-startNs) +"ns");
+    }
+
+    public static class Dice implements Procedure{
+
+        @Override
+        public void run() {
+            // 코드 조각 시작
+            int randomValue = new Random().nextInt(6)+1;
+            System.out.println("randomValue = " + randomValue);
+            //코드 조각 종료
+        }
+    }
+
+    public static class Sum implements Procedure{
+
+        @Override
+        public void run() {
+            // 코드 조각 시작
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("i = " + i);
+            }
+            //코드 조각 종료
+        }
+    }
+    public static void helloDice(){
+        long startNs = System.nanoTime();
+
+        // 코드 조각 시작
+        int randomValue = new Random().nextInt(6)+1;
+        System.out.println("randomValue = " + randomValue);
+        //코드 조각 종료
+
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간: " + (endNs-startNs) +"ns");
+    }
+
+    public static void helloSum(){
+        long startNs = System.nanoTime();
+
+        // 코드 조각 시작
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("i = " + i);
+        }
+        //코드 조각 종료
+
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간: " + (endNs-startNs) +"ns");
+    }
+
+    public static void main(String[] args) {
+        helloDice();
+        helloSum();
+
+        hello(new Sum());
+        hello(new Dice());
+    }
+}
